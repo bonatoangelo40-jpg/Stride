4b64e1a1f05128c673af8a7b42ec56bc176a0137
ANGELO BONATO <angelo.8956@aluno.pr.senac.br>
2025-11-25 15:58:17 -0300

    Arrumando o caminho das pastas

--- Java/inicio.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("menu-burger");
-const menu = document.getElementById("menu-lista");
-
-burger.addEventListener("click", () => {
-  menu.classList.toggle("ativo");
-});
